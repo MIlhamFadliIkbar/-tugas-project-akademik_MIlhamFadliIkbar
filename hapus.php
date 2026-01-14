d821e7bb7c0f4fe9e25c984f3cece576c2356ded
MIlhamFadliIkbar <iham58090@gmail.com>
2026-01-14 07:59:44 +0700

    Delete hapus.php

--- hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "koneksi.php";
-$nim = $_GET['nim'];
-
-mysqli_query($koneksi, "DELETE FROM mahasiswa WHERE nim='$nim'");
-
-echo "<script>alert('Data berhasil dihapus'); window.location='index.php';</script>";
-?>
